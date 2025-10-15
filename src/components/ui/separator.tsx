8c0f3f1d405428ce27f52ac1b935ac130307d6c0
Junio Chaves <68963163+13junio@users.noreply.github.com>
2025-10-14 22:56:17 -0300

    Configuração de rotas para SPA no Vercel e ajustes finais

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -6,12 +6,10 @@ import { cn } from "@/lib/utils";
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <SeparatorPrimitive.Root
     ref={ref}
-    decorative={decorative}
-    orientation={orientation}
-    className={cn("shrink-0 bg-border", orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]", className)}
+    className={cn("shrink-0 bg-border", className)}
     {...props}
   />
 ));
